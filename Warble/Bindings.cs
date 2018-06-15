476e48d1a4e82f49b221afae7157c764edf8f96a
Eric Tsai <eric@mbientlab.com>
2018-06-15 12:13:59 -0700

    Initial commit

---
+++ Warble/Bindings.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace MbientLab.Warble {
+    sealed class Bindings {
+        [StructLayout(LayoutKind.Sequential)]
+        private struct ScanMftData {
+            public IntPtr value;
+            public byte value_size;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct ScanResult {
+            [MarshalAs(UnmanagedType.LPStr)]
+            public String mac;
+            [MarshalAs(UnmanagedType.LPStr)]
+            public String name;
+            public int rssi;
+            public IntPtr private_data;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct Option {
+            [MarshalAs(UnmanagedType.LPStr)]
+            public String key;
+            [MarshalAs(UnmanagedType.LPStr)]
+            public String value;
+        }
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void FnVoid_IntPtr_WarbleGattP_CharP(IntPtr context, IntPtr gatt, [MarshalAs(UnmanagedType.LPStr)] String err);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void FnVoid_IntPtr_WarbleGattP_Int(IntPtr context, IntPtr gatt, int value);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void FnVoid_VoidP_WarbleScanResultP(IntPtr context, IntPtr result);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void FnVoid_VoidP_WarbleGattCharP_CharP(IntPtr context, IntPtr gattchar, [MarshalAs(UnmanagedType.LPStr)] String err);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void FnVoid_VoidP_WarbleGattCharP_UbyteP_Ubyte_CharP(IntPtr context, IntPtr gattchar, IntPtr value, byte length, [MarshalAs(UnmanagedType.LPStr)] String err);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void FnVoid_VoidP_WarbleGattCharP_UbyteP_Ubyte(IntPtr context, IntPtr gattchar, IntPtr value, byte length);
+
+        private const String WARBLE_DLL = "warble";
+
+        [DllImport(WARBLE_DLL, CallingConvention = CallingConvention.Cdecl)]
+        [return: MarshalAs(UnmanagedType.LPStr)]
+        public static extern String warble_lib_version();
+
+        [DllImport(WARBLE_DLL, CallingConvention = CallingConvention.Cdecl)]
+        [return: MarshalAs(UnmanagedType.LPStr)]
+        public static extern String warble_lib_config();
+
+        [DllImport(WARBLE_DLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void warble_lib_init(int length, IntPtr options);
+
+        [DllImport(WARBLE_DLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void warble_scanner_stop();
+
+        [DllImport(WARBLE_DLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void warble_scanner_start(int length, IntPtr options);
+
+        [DllImport(WARBLE_DLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void warble_scanner_set_handler(IntPtr context, FnVoid_VoidP_WarbleScanResultP handler);
+
+        [DllImport(WARBLE_DLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr warble_scan_result_get_manufacturer_data(IntPtr result, ushort companyId);
+
+        [DllImport(WARBLE_DLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int warble_scan_result_has_service_uuid(IntPtr result, [MarshalAs(UnmanagedType.LPStr)] String uuid);
+
+        [DllImport(WARBLE_DLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void warble_gatt_connect_async(IntPtr gatt, IntPtr context, FnVoid_IntPtr_WarbleGattP_CharP handler);
+
+        [DllImport(WARBLE_DLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void warble_gatt_disconnect(IntPtr gatt);
+
+        [DllImport(WARBLE_DLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void warble_gatt_delete(IntPtr gatt);
+
+        [DllImport(WARBLE_DLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void warble_gatt_on_disconnect(IntPtr gatt, IntPtr context, FnVoid_IntPtr_WarbleGattP_Int handler);
+
+        [DllImport(WARBLE_DLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int warble_gatt_is_connected(IntPtr gatt);
+
+        [DllImport(WARBLE_DLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr warble_gatt_create([MarshalAs(UnmanagedType.LPStr)] String mac);
+
+        [DllImport(WARBLE_DLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr warble_gatt_create_with_options(int length, IntPtr options);
+
+        [DllImport(WARBLE_DLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr warble_gatt_find_characteristic(IntPtr gatt, [MarshalAs(UnmanagedType.LPStr)] String uuid);
+
+        [DllImport(WARBLE_DLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int warble_gatt_has_service(IntPtr gatt, [MarshalAs(UnmanagedType.LPStr)] String uuid);
+
+        [DllImport(WARBLE_DLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void warble_gattchar_disable_notifications_async(IntPtr gattchar, IntPtr context, FnVoid_VoidP_WarbleGattCharP_CharP handler);
+
+        [DllImport(WARBLE_DLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void warble_gattchar_write_without_resp_async(IntPtr gattchar, byte[] value, byte value_size, IntPtr context, FnVoid_VoidP_WarbleGattCharP_CharP handler);
+
+        [DllImport(WARBLE_DLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void warble_gattchar_read_async(IntPtr gattchar, IntPtr context, FnVoid_VoidP_WarbleGattCharP_UbyteP_Ubyte_CharP handler);
+
+        [DllImport(WARBLE_DLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void warble_gattchar_write_async(IntPtr gattchar, byte[] value, byte value_size, IntPtr context, FnVoid_VoidP_WarbleGattCharP_CharP handler);
+
+        [DllImport(WARBLE_DLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void warble_gattchar_enable_notifications_async(IntPtr gattchar, IntPtr context, FnVoid_VoidP_WarbleGattCharP_CharP handler);
+
+        [DllImport(WARBLE_DLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void warble_gattchar_on_notification_received(IntPtr gattchar, IntPtr context, FnVoid_VoidP_WarbleGattCharP_UbyteP_Ubyte handler);
+
+        [DllImport(WARBLE_DLL, CallingConvention = CallingConvention.Cdecl)]
+        [return: MarshalAs(UnmanagedType.LPStr)]
+        public static extern String warble_gattchar_get_uuid(IntPtr gattchar);
+
+        [DllImport(WARBLE_DLL, CallingConvention = CallingConvention.Cdecl)]
+        [return: MarshalAs(UnmanagedType.LPStr)]
+        public static extern IntPtr warble_gattchar_get_gatt(IntPtr gattchar);
+
+    }
+}
